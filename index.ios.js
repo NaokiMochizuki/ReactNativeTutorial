957e1e1143413f39734f07822a1c53218059a2f0
NaokiMochizuki <naoki.mochizuki1006@gmail.com>
2017-07-27 14:18:03 +0900

    setStateを用いてstateの値を1秒ごとに書き換え ~state~

--- index.ios.js
+++ index.ios.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
 import { AppRegistry, Text, View } from 'react-native';
 
-export default class LotsOfGreeting extends Component {
+export default class BlinkApp extends Component {
   render() {
-    return(
-      <View style={{ alignItems: 'center' }}>
-        <Greeting name='Rexxar' />
-        <Greeting name='Jaina' />
-        <Greeting name='Valeera' />
+    return (
+      <View>
+        <Blink text='I love to blink' />
+        <Blink text='Yes blinking is so great' />
+        <Blink text='Why did they ever take this out of HTML' />
+        <Blink text='Look at me look at me look at me' />
       </View>
     );
   }
 }
 
-class Greeting extends Component {
-  render(){
-    return(
-      <Text>Hello { this.props.name }!</Text>
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { showText: true };
+
+    //1秒ごとにsetStateでstateの値を書き換える処理
+    setInterval(() => {
+      this.setState(previousState => {
+        return { showText: !previousState.showText };
+      });
+    }, 1000);
+  }
+
+  render() {
+    let display = this.state.showText ? this.props.text : ' ';
+    return (
+      <Text>{ display }</Text>
     );
   }
 }
 
 
-AppRegistry.registerComponent('ReactNativeTutorial', () => LotsOfGreeting);
+AppRegistry.registerComponent('ReactNativeTutorial', () => BlinkApp);
