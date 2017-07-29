0b7e28a5dba5ca460ad5063a5abb68ff552fff82
NaokiMochizuki <naoki.mochizuki1006@gmail.com>
2017-07-29 14:44:40 +0900

    ScrollViewにpaginateEnableとhorizontal propsの指定

--- index.ios.js
+++ index.ios.js
@@ -5,7 +5,9 @@ export default class IScrolledDownAndWhatHappenedNextShockedMe extends Component
   render() {
     return(
       //ScrollViewでスクロール表示の作成
-      <ScrollView>
+      //paginateEnabledで、ページ単位での移動(スクロール)をするかどうか指定
+      //horizontalで、横方向のスクロール表示にするかどうか指定
+      <ScrollView pagingEnabled={ true } horizontal={ true }>
         <Text style={{ fontSize: 96 }}>Scroll me plz</Text>
         <Image source={ require('./img/favicon.png') } />
         <Image source={ require('./img/favicon.png') } />
