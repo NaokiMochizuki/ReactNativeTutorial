4227b2db4c9a0e1b9238ed60f9283b24c7153ecc
NaokiMochizuki <naoki.mochizuki1006@gmail.com>
2017-07-29 14:58:58 +0900

    FlatListによるリスト表示 ~Using List View~

--- index.ios.js
+++ index.ios.js
@@ -1,26 +1,38 @@
 import React, { Component } from 'react';
-import { AppRegistry, ScrollView, Image, Text } from 'react-native';
+import { AppRegistry, FlatList, StyleSheet, Text, View } from 'react-native';
 
-export default class IScrolledDownAndWhatHappenedNextShockedMe extends Component {
+export default class FlatListBasics extends Component {
   render() {
+    //FlatListでリストの表示。dataとrenderItemは必須
     return(
-      //ScrollViewでスクロール表示の作成
-      //paginateEnabledで、ページ単位での移動(スクロール)をするかどうか指定
-      //horizontalで、横方向のスクロール表示にするかどうか指定
-      <ScrollView pagingEnabled={ true } horizontal={ true }>
-        <Text style={{ fontSize: 96 }}>Scroll me plz</Text>
-        <Image source={ require('./img/favicon.png') } />
-        <Image source={ require('./img/favicon.png') } />
-        <Image source={ require('./img/favicon.png') } />
-        <Image source={ require('./img/favicon.png') } />
-        <Text style={{ fontSize: 96 }}>Scroll down</Text>
-        <Image source={ require('./img/favicon.png') } />
-        <Image source={ require('./img/favicon.png') } />
-        <Image source={ require('./img/favicon.png') } />
-        <Image source={ require('./img/favicon.png') } />
-      </ScrollView>
+      <View style={ styles.container }>
+        <FlatList
+          data={ [
+            { key: 'Devin' },
+            { key: 'Jackson' },
+            { key: 'James' },
+            { key: 'Joel' },
+            { key: 'John' },
+            { key: 'Jillian' },
+            { key: 'Jimmy' },
+            { key: 'Julie' }
+          ]}
+          renderItem={ ({ item }) => <Text style={ styles.item }>{ item.key }</Text> } />
+      </View>
     );
   }
 }
 
-AppRegistry.registerComponent('ReactNativeTutorial', () => IScrolledDownAndWhatHappenedNextShockedMe);
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+})
+
+AppRegistry.registerComponent('ReactNativeTutorial', () => FlatListBasics);
